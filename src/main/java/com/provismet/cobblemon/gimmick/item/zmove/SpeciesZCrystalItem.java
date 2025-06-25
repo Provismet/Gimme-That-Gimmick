ddde0ba016e243cc9233f54f4fc1b44eb28cd364
Provismet <17149901+Provismet@users.noreply.github.com>
2025-06-25 02:30:26 +0100

    Fix Arceus Z-Crystals.

--- src/main/java/com/provismet/cobblemon/gimmick/item/zmove/SpeciesZCrystalItem.java
+++ src/main/java/com/provismet/cobblemon/gimmick/item/zmove/SpeciesZCrystalItem.java
@@ -1,6 +1,7 @@
 package com.provismet.cobblemon.gimmick.item.zmove;
 
 import com.cobblemon.mod.common.api.types.ElementalType;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import eu.pb4.polymer.resourcepack.api.PolymerModelData;
 import net.minecraft.item.Item;
 
@@ -14,5 +15,15 @@ public class SpeciesZCrystalItem extends TypedZCrystalItem {
 
     public SpeciesZCrystalItem (Settings settings, Item baseVanillaItem, PolymerModelData modelData, int tooltipLines, ElementalType type) {
         super(settings, baseVanillaItem, modelData, tooltipLines, type);
+    }
+
+    @Override
+    public void giveToPokemon (Pokemon pokemon) {
+
+    }
+
+    @Override
+    public void removeFromPokemon (Pokemon pokemon) {
+
     }
 }
