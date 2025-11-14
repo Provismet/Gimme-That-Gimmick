769b8b3a6fd099427fb6673d09794da93f52757b
Provismet <17149901+Provismet@users.noreply.github.com>
2025-11-14 18:16:50 +0000

    Make mod run in singleplayer

--- src/main/java/com/provismet/cobblemon/gimmick/GimmeThatGimmickMain.java
+++ src/main/java/com/provismet/cobblemon/gimmick/GimmeThatGimmickMain.java
@@ -1,6 +1,23 @@
 package com.provismet.cobblemon.gimmick;
 
+import com.provismet.cobblemon.gimmick.commands.GTGCommands;
+import com.provismet.cobblemon.gimmick.handlers.BattleEventHandler;
+import com.provismet.cobblemon.gimmick.handlers.CobblemonEventHandler;
+import com.provismet.cobblemon.gimmick.handlers.DynamaxEventHandler;
+import com.provismet.cobblemon.gimmick.handlers.UltraBurstEventHandler;
+import com.provismet.cobblemon.gimmick.registry.GTGBlocks;
+import com.provismet.cobblemon.gimmick.registry.GTGDynamicRegistries;
+import com.provismet.cobblemon.gimmick.registry.GTGDynamicRegistryKeys;
+import com.provismet.cobblemon.gimmick.registry.GTGEnchantmentComponents;
+import com.provismet.cobblemon.gimmick.registry.GTGItemDataComponents;
+import com.provismet.cobblemon.gimmick.registry.GTGItemGroup;
+import com.provismet.cobblemon.gimmick.registry.GTGItems;
+import com.provismet.cobblemon.gimmick.registry.GTGStatusEffects;
+import com.provismet.cobblemon.gimmick.util.DelayedTicker;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +32,24 @@ public class GimmeThatGimmickMain implements ModInitializer {
 
     @Override
     public void onInitialize () {
+        PolymerResourcePackUtils.addModAssets(GimmeThatGimmickMain.MODID);
 
+        GTGDynamicRegistryKeys.register();
+
+        GTGItems.init();
+        GTGBlocks.init();
+        GTGItemDataComponents.init();
+        GTGEnchantmentComponents.init();
+        GTGStatusEffects.init();
+        GTGItemGroup.register();
+        GTGCommands.register();
+
+        CobblemonEventHandler.register();
+        UltraBurstEventHandler.register();
+        DynamaxEventHandler.register();
+        BattleEventHandler.register();
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> GTGDynamicRegistries.loadRegistries(server.getRegistryManager()));
+        ServerTickEvents.START_SERVER_TICK.register(server -> DelayedTicker.runAll());
     }
 }
